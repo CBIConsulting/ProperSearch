e3bc9f2c25a138bb87bbf88e160b0d1da35cd7dd
sebastiancbvz <sebastian.cabanas@cbiconsulting.es>
2016-04-06 20:51:25 +0200

    Add a lot of configurable settings and new functionality

--- examples/jsx/example.js
+++ examples/jsx/example.js
@@ -1,15 +1,106 @@
 import Search from "../../src/jsx/propersearch";
+import Normalizer from "normalizer";
+import $ from "jquery";
 
-const body = document.getElementById('canvas');
-const data = [];
+const search = document.getElementById('canvas');
+const button = document.getElementById('canvas2');
 
-for (var i = 10000; i >= 0; i--) {
-	data.push({value: 'item-' + i, label: 'Item ' + i});
-};
+let data = [];
+let selection = null;
+let language = 'ENG';
+let defaultSearch = '';
+let multiSelect = true;
+let listHeight = 200;
+let listRowHeight = 26;
 
-const afterSelect = (data, selection) => {
-	console.info(data);
-	console.info(selection);
+const buttonClick = (e, name) => {
+	console.log('Button ' + name + ' has been clicked');
 }
 
-ReactDOM.render(<Search data={data} multiSelect={true} afterSelect={afterSelect}/>, body);
+const filter = (listElement, value) => {
+	let data = listElement.name.toLowerCase();
+	data = Normalizer.normalize(data);
+	return data.indexOf(value) >= 0;
+}
+
+const formater = listElement => {
+	return <button className ="btn btn-default" onClick={ (e) => {buttonClick(e, listElement.name)} }>{ listElement.name }</button>;
+}
+
+const afterSelect = (data, selection) => {
+	console.info('Data: ', data);
+	console.info('Selection: ', selection);
+}
+
+const afterSearch = value => {
+	console.info('Search: ', value);
+}
+
+for (let i = 10; i >= 0; i--) {
+	data.push({itemID: 'item-' + i, display: formater, name: 'Tést ' + i,moreFields: 'moreFields values'});
+};
+
+// Modifying data with the fields
+$( document ).ready(function() {
+    $('#multiselect_id').on("change",(e) => {
+    	if(e.target.value == '0') multiSelect = false;
+    	else multiSelect = true;
+    });
+    console.log(multiSelect)
+});
+
+ReactDOM.render(
+	<div>
+		<Search
+			data={data}
+			idField={'itemID'}
+			displayField={'display'}
+			lang={language}
+			filter={filter}
+			multiSelect={multiSelect}
+			defaultSelection={selection}
+			defaultSearch={defaultSearch}
+			placeholder={'Search placeHolder'}
+			afterSelect={afterSelect}
+			afterSearch={afterSearch}
+		/>
+	</div>,
+	search
+);
+
+const changeData = e => {
+	data = [];
+
+	let i = Math.floor(Math.random()* 10);
+	selection = ['item-'+i,'item-'+(i+1)];
+	defaultSearch = 'Item '+i;
+
+	listHeight += i;
+	listRowHeight += i;
+	multiSelect = !multiSelect;
+
+	if (i % 2 == 0) language = 'ENG';
+	else language = 'ESP';
+
+	for (let i = (Math.floor(Math.random()* 1000) + 10); i >= 0; i--) {
+		data.push({value: 'item-' + i, label: 'Item ' + i});
+	}
+
+	ReactDOM.render(
+		<div>
+			<Search
+				data={data}
+				lang={language}
+				multiSelect={multiSelect}
+				defaultSelection={selection}
+				defaultSearch={defaultSearch}
+				placeholder={'Another placeholder ' + i}
+				afterSelect={afterSelect}
+				afterSearch={afterSearch}
+			/>
+		</div>,
+		search
+	);
+}
+
+ReactDOM.render(<button className="btn btn-default" onClick={changeData}> Random Data </button>, button);
--- src/jsx/components/searchList.js
+++ src/jsx/components/searchList.js
@@ -5,20 +5,43 @@ import { VirtualScroll } from 'react-virtualized';
 import Dimensions from 'react-dimensions';
 const Set = require('es6-set');
 
+// For more info about this read ReadMe.md
 function getDefaultProps() {
 	return {
-		data: [],
+		data: null,
+		rawData: null, // Just when you use a function as a display field. (Inmutable same as received by the main component)
+		indexedData: null, // Just when you use a function as a display field. (array) (Full indexed data not filted)
 		onSelectionChange: null,
-		multiSelect: true,
+		multiSelect: false,
 		messages: null,
 		selection: new Set(),
 		allSelected: false,
 		listRowHeight: 26,
 		listHeight: 200,
-		listWidth: null // Container width by default
+		listWidth: null, // Container width by default
+		idField: 'value',
+		displayField: 'label',
+		showIcon: true,
+		listElementClass: null
 	}
 }
 
+/**
+ * A Component that render a list of selectable items, with single or multiple selection and return the selected items each time a new item be selected.
+ *
+ * Simple example usage:
+ *	let handleSelection = function(selection){
+ *		console.log('Selected values: ' + selection) // The selection is a Set obj
+ *	}
+ *
+ * 	<SearchList
+ *		data={data} // As an Inmutable obj
+ *		idField={'value'} // Field used as an id
+ *		onSelectionChange={handleSelection}
+ * 		multiselect={false}
+ *	/>
+ * ```
+ */
 class SearchList extends React.Component {
 	constructor(props){
 		super(props);
@@ -31,25 +54,39 @@ class SearchList extends React.Component {
 
 	shouldComponentUpdate(nextProps, nextState){
 		let propschanged = !shallowEqualImmutable(this.props, nextProps);
-		let statechanged = !shallowEqualImmutable(this.state, nextState);
-		let somethingchanged = propschanged || statechanged;
 
 		if (propschanged) {
-			let nothing =  nextProps.selection.size == 0;
+			let nothingSelected = false;
+			propschanged = shallowEqualImmutable(this.props.selection, nextProps.selection); // If selection change don't render
 
-			if (nextProps.allSelected != this.state.allSelected || nothing != this.state.nothingSelected) {
+			if (!nextProps.allSelected) nothingSelected = this.isNothingSelected(nextProps.data, nextProps.selection);
+
+			// When the props change update the state.
+			if (nextProps.allSelected != this.state.allSelected || nothingSelected != this.state.nothingSelected) {
 				this.setState({
 					allSelected: nextProps.allSelected,
-					nothingSelected: nothing,
+					nothingSelected: nothingSelected,
 				});
+
+				return true;
 			}
 		}
 
-		return somethingchanged;
+		return propschanged;
 	}
 
+/**
+ * Function called each time an element of the list is selected. Get the value (value of the idField) of the
+ * element that was selected, them change the selection and call to onSelectionChange function in the props sending
+ * the new selection.
+ *
+ * @param (String)	itemValue 	Value of the idField of the selected element
+ * @param (Array)	e 			Element which call the function
+ */
 	handleElementClick(itemValue, e) {
-		let data = this.props.data, selection = this.props.selection, nothingSelected = null;
+		e.preventDefault();
+
+		let data = this.props.data, selection = this.props.selection, nothingSelected = false, allSelected = false;
 
 		if (this.props.multiSelect) {
 			if (selection.has(itemValue)) {
@@ -58,13 +95,17 @@ class SearchList extends React.Component {
 				selection.add(itemValue);
 			}
 		} else {
-			selection = new Set([itemValue]);
+			if (selection.has(itemValue)) selection = new Set();
+			else selection = new Set([itemValue]);
 		}
 
-		nothingSelected = selection.size == 0;
-		if (selection.size != data.size || nothingSelected != this.state.nothingSelected){
+		allSelected = this.isAllSelected(data, selection);
+		if (!allSelected) nothingSelected = this.isNothingSelected(data, selection);
+
+		// If the state has changed update it
+		if (allSelected != this.state.allSelected || nothingSelected != this.state.nothingSelected){
 			this.setState({
-				allSelected: selection.size == data.size,
+				allSelected: allSelected,
 				nothingSelected: nothingSelected
 			});
 		}
@@ -74,20 +115,67 @@ class SearchList extends React.Component {
 		}
 	}
 
+/**
+ * Check if all the current data are not selected
+ *
+ * @param {array}	data		The data to compare with selection
+ * @param {object}	selection	The current selection Set of values (idField)
+ */
+	isNothingSelected(data, selection) {
+		let result = true;
+		if (selection.size == 0) return true;
+
+		data.forEach(element => {
+			if (selection.has(element.get(this.props.idField, null))) { // Some data not in selection
+				result = false;
+				return false;
+			}
+		});
+
+		return result;
+	}
+
+/**
+ * Check if all the current data are selected.
+ *
+ * @param {array}	data		The data to compare with selection
+ * @param {object}	selection	The current selection Set of values (idField)
+ */
+	isAllSelected(data, selection) {
+		let result = true;
+		if (data.size > selection.size) return false;
+
+		data.forEach(element => {
+			if (!selection.has(element.get(this.props.idField, null))) { // Some data not in selection
+				result = false;
+				return false;
+			}
+		});
+
+		return result;
+	}
+
+/**
+ * Function called each time the buttons in the bar of the list has been clicked. Delete or add all the data elements into the selection, just if it has changed.
+ * Prevent multiple clicks in the same button.
+ *
+ * @param (Boolean)	selectAll 	If its a select all action or an unselect all.
+ * @param (Array)	e 			Element which call the function
+ */
 	handleSelectAll(selectAll, e) {
 		e.preventDefault();
 
-		let newData = [], data = this.props.data;
+		let newData = [], data = this.props.data, field = this.props.idField;
 		let selection = this.props.selection;
 		let hasChanged = (selectAll != this.state.allSelected || (!selectAll && !this.state.nothingSelected)); // nothingSelected = false then something is selected
 
 		if (selectAll && hasChanged) {
-			data.forEach(item => {
-				selection.add(item.get('value', null));
+			data.forEach(element => {
+				selection.add(element.get(field, null));
 			});
 		} else {
-			data.forEach(item => {
-				selection.delete(item.get('value', null));
+			data.forEach(element => {
+				selection.delete(element.get(field, null));
 			});
 		}
 
@@ -101,18 +189,22 @@ class SearchList extends React.Component {
 		if (typeof this.props.onSelectionChange == 'function' && hasChanged) {
 			this.props.onSelectionChange.call(this, selection);
 		}
+	}
 
-	};
-
+/**
+ * Return the tool bar for the top of the list. It will be displayed only when the selection can be multiple.
+ *
+ * @return (html) 	The toolbar code
+ */
 	getToolbar() {
 		return (
-			<div className="search-list-bar">
+			<div className="proper-search-list-bar">
 				<div className="btn-group form-inline">
-					<a id="search-list-bar-check" className="btn" role="button" onClick={this.handleSelectAll.bind(this, true)}>
+					<a id="proper-search-list-bar-check" className="btn" role="button" onClick={this.handleSelectAll.bind(this, true)}>
 						<label>{this.props.messages.all}</label>
 					</a>
 					&nbsp;
-					<a id="search-list-bar-unCheck" className="btn" role="button" onClick={this.handleSelectAll.bind(this, false)}>
+					<a id="proper-search-list-bar-unCheck" className="btn" role="button" onClick={this.handleSelectAll.bind(this, false)}>
 						<label>{this.props.messages.none}</label>
 					</a>
 				</div>
@@ -120,47 +212,81 @@ class SearchList extends React.Component {
 		);
 	}
 
+/**
+ * Build and return the content of the list.
+ */
 	getContent() {
-		let content =  null, result = [], addClass = '';
-		let data = this.props.data, selection = this.props.selection;
+		let icon =  null, result = [], selectedClass = null, className = null, element = null, content = null;
+		let data = this.props.data, selection = this.props.selection, field = this.props.idField;
+		let displayField = this.props.displayField, showIcon = this.props.showIcon;
+		let listElementClass = this.props.listElementClass;
 
 		data.forEach((item, index) => {
-			if (this.props.multiSelect) {
+			element = item.get(displayField);
+			className = "proper-search-list-element";
 
-				if (item.get('_selected', false)) {
-					content = <i className="fa fa-check-square-o"/>;
-					addClass = ' selected'
-				} else {
-					content = <i className="fa fa-square-o"/>;
-					addClass = '';
+			if (this.props.multiSelect) {
+				if (showIcon) {
+					if (item.get('_selected', false)) {
+						icon = <i className="fa fa-check-square-o"/>;
+						selectedClass = ' proper-search-selected'
+					} else {
+						icon = <i className="fa fa-square-o"/>;
+						selectedClass = null;
+					}
 				}
 			} else {
-				if (item.get('_selected')) addClass = ' single-selected';
-				else addClass = '';
+				if (item.get('_selected')) selectedClass = ' proper-search-single-selected';
+				else selectedClass = null;
 			}
 
-			result.push(
-				<div key={'element-' + index} className={"search-list-element" + addClass} onClick={this.handleElementClick.bind(this, item.get('value'))}>
-					{content}
-					&nbsp;&nbsp;
-					{item.get('label')}
+			if (listElementClass) {
+				className += ' ' + listElementClass;
+			}
+
+			if (selectedClass) {
+				className += ' ' + selectedClass;
+			}
+
+			if (typeof element == 'function') {
+				let id = item.get(field);
+				element = element(this.props.indexedData[id]);
+			}
+
+			content = (
+				<div key={'element-' + index} className={className} onClick={this.handleElementClick.bind(this, item.get(field))}>
+					{icon}
+					{element}
 				</div>
 			);
+
+			result.push(content);
 		});
 
 		return result;
 	}
-
+/**
+ * To be rendered when the data has no data (Ex. filtered data)
+ *
+ * @return (html) An div with a message
+ */
 	noRowsRenderer () {
-	    return <div className={"search-list search-list-no-data"}>{this.props.messages.noData}</div>;
+	    return <div className={"proper-search-list search-list-no-data"}>{this.props.messages.noData}</div>;
 	}
 
+/**
+ * Function called to get the content of each element of the list.
+ *
+ * @param 	list 		List of elements builded on getContent.
+ * @param 	index 		Current index to be rendered.
+ * @return 	element 	The element on the index position
+ */
 	rowRenderer(list, index) {
 		return list[index];
 	}
 
 	render(){
-		let toolbar = null, rowsCount = 0, list = this.getContent(), className = "search-list";
+		let toolbar = null, rowsCount = 0, list = this.getContent(), className = "proper-search-list";
 
 		if (this.props.multiSelect) toolbar = this.getToolbar();
 		rowsCount = this.props.data.size;
@@ -173,7 +299,7 @@ class SearchList extends React.Component {
 			<div className={className}>
 				{toolbar}
 				<VirtualScroll
-					className={"search-list-virtual"}
+					className={"proper-search-list-virtual"}
 	                width={this.props.listWidth || this.props.containerWidth}
 	                height={this.props.listHeight}
 	                rowRenderer={this.rowRenderer.bind(this, list)}
