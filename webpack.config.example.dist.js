48e09f7a6163dee21ce3b944d544bfa4ce6e4e3f
sebastiancbvz <sebastian.cabanas@cbiconsulting.es>
2016-04-08 14:55:15 +0200

    Update example app page

---
+++ webpack.config.example.dist.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  context: path.join(__dirname, 'examples'),
+  entry: {
+    javascript: "./jsx/example.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ["babel-loader"],
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loaders: ["babel-loader"],
+      },
+      {
+        test: /\.html$/,
+        loader: "file?name=[name].[ext]",
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('css!sass?includePaths[]='+path.resolve(__dirname, "./node_modules/compass-mixins/lib"))
+      }
+    ],
+  },
+  output: {
+    libraryTarget: "var",
+    library: "App",
+    filename: "app.js",
+    path: __dirname + "/examples/js"
+  },
+  plugins: [
+    new ExtractTextPlugin('propersearch.css', {
+      allChunks: true
+    }),
+    new webpack.optimize.DedupePlugin()
+  ]
+}
