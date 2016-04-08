cbe4489a47bc8a30f8de2aafc3bc420d2ed0e458
sebastiancbvz <sebastian.cabanas@cbiconsulting.es>
2016-04-08 12:42:13 +0200

    Merge remote-tracking branch 'origin/dev' into dev (reverted from commit e919c2540000794dae2ceae29f85baa1f56b9115) (reverted from commit 4364dd057d83ba251e0ce101d52958d44ddfb7be)

---
+++ examples/js/example.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var _app = require("./app");
+
+var _app2 = _interopRequireDefault(_app);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var body = document.getElementById('body');
+ReactDOM.render(React.createElement(_app2["default"], null), body);
